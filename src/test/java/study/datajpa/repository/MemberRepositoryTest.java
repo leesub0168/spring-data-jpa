75e039c022c504d20460e14fd43d621876a80ad9
leeso <leesub0168@gmail.com>
2023-06-29 21:55:33 +0900

    jpa 기반 리포지토리 메소드 -> 스프링 data jpa로 변경시 적용 테스트

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -7,6 +7,8 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -30,6 +32,34 @@ class MemberRepositoryTest {
         assertEquals(member.getId(), findMember.getId());
         assertEquals(member.getUsername(), findMember.getUsername());
         assertEquals(member, findMember);
+    }
+    @Test
+    public void basicCRUD() throws Exception {
+        //given
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        //when
+        Member findMember1 = memberRepository.findById(member1.getId()).get();
+        Member findMember2 = memberRepository.findById(member2.getId()).get();
+
+        //then
+        assertEquals(findMember1, member1);
+        assertEquals(findMember2, member2);
+
+        List<Member> all = memberRepository.findAll();
+        assertEquals(2, all.size());
+
+        long count = memberRepository.count();
+        assertEquals(2, count);
+
+        memberRepository.delete(member1);
+        memberRepository.delete(member2);
+
+        long afterDelete = memberRepository.count();
+        assertEquals(0, afterDelete);
     }
 
 }
