d3f9b855bf8597b432a9d2ff718096e977072e8c
leeso <leesub0168@gmail.com>
2023-06-29 20:56:40 +0900

    Spring Data Jpa 테스트

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -3,16 +3,19 @@ package study.datajpa.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
-class MemberJpaRepositoryTest {
+@Rollback(value = false)
+class MemberRepositoryTest {
+
     @Autowired
-    MemberJpaRepository memberJpaRepository;
+    MemberRepository memberRepository;
 
     @Test
     public void testMember() throws Exception {
@@ -20,12 +23,13 @@ class MemberJpaRepositoryTest {
         Member member = new Member("memberA");
 
         //when
-        Member saveMember = memberJpaRepository.save(member);
-        Member findMember = memberJpaRepository.find(saveMember.getId());
+        Member saveMember = memberRepository.save(member);
+        Member findMember = memberRepository.findById(saveMember.getId()).get();
 
         //then
         assertEquals(member.getId(), findMember.getId());
         assertEquals(member.getUsername(), findMember.getUsername());
         assertEquals(member, findMember);
     }
+
 }
